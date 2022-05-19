3f21a8bbd5ad443cc404ff1ada61e21c53f81f0c
T-S1 <yama153shou@gmail.com>
2022-05-19 21:27:03 +0900

    visualize motion

--- dtw.py
+++ dtw.py
@@ -1,10 +1,12 @@
 # import pdb; pdb.set_trace()
 import argparse
 import os
-from typing import List, Tuple
 import json
+from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
@@ -17,41 +19,138 @@ def trans_coord(arr_pos):
 
     arr_pos_trans = np.zeros_like(arr_pos)
     pel = arr_pos[:, kinect.PELVIS, :]
+    nav = arr_pos[:, kinect.SPINE_NAVAL, :]
     hip_l = arr_pos[:, kinect.HIP_LEFT, :]
     hip_r = arr_pos[:, kinect.HIP_RIGHT, :]
-    hip_l2r = hip_r - hip_l
 
-    for joint in range(kinect.NUM_JOINTS):
-        arr_pos_trans[:, joint, :] = arr_pos[:, joint, :] - pel
+    e_x = hip_r - hip_l
+    e_x /= np.linalg.norm(e_x, ord=2, axis=1, keepdims=True)
+    e_y = np.cross(nav - pel, e_x)
+    e_y /= np.linalg.norm(e_y, ord=2, axis=1, keepdims=True)
+    e_z = np.cross(e_x, e_y)
+    rot_mat = np.stack([e_x, e_y, e_z], axis=1)     # (T, 3, 3)
+    rot_mat = np.expand_dims(rot_mat, axis=1)
+
+    arr_pos_temp = np.expand_dims(arr_pos, axis=3)      # (T, J, 3, 1)
+    arr_pos_temp = np.matmul(rot_mat, arr_pos_temp)
+    arr_pos_temp = np.squeeze(arr_pos_temp)             # (T, J, 3)
+    pel_trans = arr_pos_temp[:, kinect.PELVIS, :]       # (T, 3)
+    arr_pos_temp -= np.expand_dims(pel_trans, axis=1)
+
+    arr_pos_trans[:, :, :] = arr_pos_temp
 
     return arr_pos_trans
 
 
-class Visualizer():
-    def __init__(self):
-        fig, self.ax = plt.subplots(
-            tight_layout=True
-        )
+class MotionDrawer():
+    def __init__(self, arr_dist, path_list):
 
-    def vis_scores(self, output_jpg: str, arr_dist: np.ndarray) -> None:
+        self.temp_dir = "./temp"
+        if not os.path.isdir(self.temp_dir):
+            os.makedirs(self.temp_dir)
 
-        self.ax.bar(kinect.JOINT_NAME_LIST, arr_dist)
+        self.dim_x = 0
+        self.dim_y = 1
+        self.dim_z = 2
+        self.alpha = 1000    # [mm]
+        self.width = 512
+        self.height = 512
+
+        self.arr_dist = arr_dist
+        self.path_list = path_list
+
+        self.fig = plt.figure(tight_layout=True)
+        self.ax = self.fig.add_subplot(projection="3d")
+
+    def draw_frame(self, arr_pos, idx):
+
+        pel = arr_pos[idx, kinect.PELVIS]
+        self.ax.set(xlim3d=(pel[self.dim_x] - self.alpha, pel[self.dim_x] + self.alpha))
+        self.ax.set(ylim3d=(pel[self.dim_y] - self.alpha, pel[self.dim_y] + self.alpha))
+        self.ax.set(zlim3d=(pel[self.dim_z] - self.alpha, pel[self.dim_z] + self.alpha))
+
+        for joint_1, joint_2 in kinect.bone_list:
+            pos_1 = arr_pos[idx, joint_1]
+            pos_2 = arr_pos[idx, joint_2]
+            self.ax.plot(
+                [pos_1[self.dim_x], pos_2[self.dim_x]],
+                [pos_1[self.dim_y], pos_2[self.dim_y]],
+                [pos_1[self.dim_z], pos_2[self.dim_z]],
+                color="blue", markersize=1
+            )
+
+        plt.savefig(f"{self.temp_dir}/temp.jpg")
+        self.ax.cla()
+
+        im = cv2.imread(f"{self.temp_dir}/temp.jpg")
+        if np.shape(im)[0] < np.shape(im)[1]:
+            width_temp = round(im.shape[1] * self.height / im.shape[0])
+            im = cv2.resize(im, (width_temp, self.height))
+            left = (self.width - self.height) // 2
+            right = (self.width + self.height) // 2
+            im = im[:, left: right]
+        else:
+            height_temp = round(im.shape[0] * self.width / im.shape[1])
+            im = cv2.resize(im, (self.width, height_temp))
+            top = (self.height - self.width) // 2
+            bottom = (self.height + self.width) // 2
+            im = im[top: bottom, :]
+
+        return im
+
+    def vis_warped_motion(self, arr_pos_1, arr_pos_2, output_mp4, fps, target_joint=kinect.WRIST_RIGHT):
+
+        print("Processing")
+
+        fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+        writer = cv2.VideoWriter(output_mp4, fourcc, fps, (self.width * 2, self.height))
+
+        for i, j in tqdm(self.path_list[target_joint]):
+
+            im_1 = self.draw_frame(arr_pos_1, i)
+            im_2 = self.draw_frame(arr_pos_2, j)
+
+            im = np.concatenate([im_1, im_2], axis=1)
+
+            cv2.putText(
+                im, f"(i, j) = ({i}, {j})", (0, self.height),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv2.LINE_4
+            )
+
+            writer.write(im)
+
+        writer.release()
+
+
+class DTWDrawer():
+    def __init__(self, arr_dist, path_list):
+
+        self.arr_dist = arr_dist
+        self.path_list = path_list
+
+        self.fig, self.ax = plt.subplots(tight_layout=True)
+
+    def vis_scores(self, output_jpg):
+
+        self.ax.barh(kinect.JOINT_NAME_LIST, self.arr_dist, alpha=0)
         plt.savefig(output_jpg)
         plt.cla()
 
-    def vis_paths(self, out_jpg_dir: str, path_list: List[List[int]]) -> None:
+    def vis_paths(self, out_jpg_dir):
 
-        i_max = path_list[0][-1][0]
-        j_max = path_list[0][-1][1]
+        i_max = self.path_list[0][-1][0]
+        j_max = self.path_list[0][-1][1]
         x = np.zeros((i_max + 1, j_max + 1))
 
         for joint in range(kinect.NUM_JOINTS):
             x[:, :] = 0
             joint_name = kinect.get_joint_name(joint)
             output_jpg = f"{out_jpg_dir}/{joint_name}.jpg"
-            for i, j in path_list[joint]:
+            for i, j in self.path_list[joint]:
                 x[i, j] = 1
             self.ax.imshow(x)
+            self.ax.set_xlabel("j")
+            self.ax.set_ylabel("i")
             plt.savefig(output_jpg)
             plt.cla()
 
@@ -75,50 +174,49 @@ def main():
     if not os.path.isdir(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+    arr_time_1, _, arr_pos_1 = kinect.read_time_ori_pos(args.input_json_1, args.body_id_1)   # (T, J, D)
+    arr_time_2, _, arr_pos_2 = kinect.read_time_ori_pos(args.input_json_2, args.body_id_2)
+
+    arr_pos_trans_1 = trans_coord(arr_pos_1)
+    arr_pos_trans_2 = trans_coord(arr_pos_2)
+
+    dist_list = []
+    path_list = []
+
+    for j in range(kinect.NUM_JOINTS):
+        dist, path = fastdtw(arr_pos_trans_1[:, j, :], arr_pos_trans_2[:, j, :], dist=euclidean)
+        dist_list.append(dist)
+        path_list.append(path)
+
     name_1 = os.path.splitext(os.path.basename(args.input_json_1))[0]
     name_2 = os.path.splitext(os.path.basename(args.input_json_2))[0]
     name = f"{name_1}-{name_2}"
     output_json = f"{OUTPUT_DIR}/{name}.json"
 
-    dist_list = []
-    path_list = []
+    with open(output_json, "w") as f:
+        json.dump(
+            {"dist": dist_list,
+             "path": path_list},
+            f, indent=4
+        )
 
-    if not os.path.isfile(output_json):
-
-        _, _, arr_pos_1 = kinect.read_time_ori_pos(args.input_json_1, args.body_id_1)   # (T, J, D)
-        _, _, arr_pos_2 = kinect.read_time_ori_pos(args.input_json_2, args.body_id_2)
-
-        arr_pos_1 = trans_coord(arr_pos_1)
-        arr_pos_2 = trans_coord(arr_pos_2)
-
-        for j in range(kinect.NUM_JOINTS):
-            dist, path = fastdtw(arr_pos_1[:, j, :], arr_pos_2[:, j, :], dist=euclidean)
-            dist_list.append(dist)
-            path_list.append(path)
-
-        with open(output_json, "w") as f:
-            json.dump(
-                {"dist": dist_list,
-                 "path": path_list},
-                f, indent=4
-            )
-    else:
-        with open(output_json, "r") as f:
-            dtw_map = json.load(f)
-
-        dist_list = dtw_map["dist"]
-        path_list = dtw_map["path"]
-
-    visualizer = Visualizer()
+    dtw_drawer = DTWDrawer(dist_list, path_list)
 
     scores_jpg = f"{OUTPUT_DIR}/scores.jpg"
-    visualizer.vis_scores(scores_jpg, dist_list)
+    dtw_drawer.vis_scores(scores_jpg)
 
     path_jpg_dir = f"{OUTPUT_DIR}/paths"
     if not os.path.isdir(path_jpg_dir):
         os.makedirs(path_jpg_dir)
 
-    visualizer.vis_paths(path_jpg_dir, path_list)
+    dtw_drawer.vis_paths(path_jpg_dir)
+
+    dif_time = arr_time_1[1:] - arr_time_1[:-1]
+    mean_dif = np.mean(dif_time) / 1e6
+    fps = 1 / mean_dif
+
+    motion_drawer = MotionDrawer(dist_list, path_list)
+    motion_drawer.vis_warped_motion(arr_pos_1, arr_pos_2, f"{OUTPUT_DIR}/compare.mp4", fps)
 
     print("Done")
 
