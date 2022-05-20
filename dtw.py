34fba4a534ca4d4de35b97a0ff7fee99b0ed784f
T-S1 <yama153shou@gmail.com>
2022-05-20 18:01:29 +0900

    tested comparing process

--- dtw.py
+++ dtw.py
@@ -43,23 +43,22 @@ def trans_coord(arr_pos):
 
 
 class MotionDrawer():
-    def __init__(self, arr_dist, path_list):
+    def __init__(self, path_list):
 
         self.temp_dir = "./temp"
         if not os.path.isdir(self.temp_dir):
             os.makedirs(self.temp_dir)
 
         self.dim_x = 0
-        self.dim_y = 1
-        self.dim_z = 2
+        self.dim_y = 2
+        self.dim_z = 1
         self.alpha = 1000    # [mm]
-        self.width = 512
-        self.height = 512
+        self.trim_cent_gap = 128
+        self.side_length = 512
 
-        self.arr_dist = arr_dist
         self.path_list = path_list
 
-        self.fig = plt.figure(tight_layout=True)
+        self.fig = plt.figure(figsize=(10, 10))
         self.ax = self.fig.add_subplot(projection="3d")
 
     def draw_frame(self, arr_pos, idx):
@@ -67,7 +66,13 @@ class MotionDrawer():
         pel = arr_pos[idx, kinect.PELVIS]
         self.ax.set(xlim3d=(pel[self.dim_x] - self.alpha, pel[self.dim_x] + self.alpha))
         self.ax.set(ylim3d=(pel[self.dim_y] - self.alpha, pel[self.dim_y] + self.alpha))
-        self.ax.set(zlim3d=(pel[self.dim_z] - self.alpha, pel[self.dim_z] + self.alpha))
+        self.ax.set(zlim3d=(pel[self.dim_z] + self.alpha, pel[self.dim_z] - self.alpha))
+
+        self.ax.scatter(
+            arr_pos[idx, :, self.dim_x],
+            arr_pos[idx, :, self.dim_y],
+            arr_pos[idx, :, self.dim_z],
+            color="blue")
 
         for joint_1, joint_2 in kinect.bone_list:
             pos_1 = arr_pos[idx, joint_1]
@@ -76,34 +81,24 @@ class MotionDrawer():
                 [pos_1[self.dim_x], pos_2[self.dim_x]],
                 [pos_1[self.dim_y], pos_2[self.dim_y]],
                 [pos_1[self.dim_z], pos_2[self.dim_z]],
-                color="blue", markersize=1
+                color="black"
             )
 
         plt.savefig(f"{self.temp_dir}/temp.jpg")
         self.ax.cla()
 
         im = cv2.imread(f"{self.temp_dir}/temp.jpg")
-        if np.shape(im)[0] < np.shape(im)[1]:
-            width_temp = round(im.shape[1] * self.height / im.shape[0])
-            im = cv2.resize(im, (width_temp, self.height))
-            left = (self.width - self.height) // 2
-            right = (self.width + self.height) // 2
-            im = im[:, left: right]
-        else:
-            height_temp = round(im.shape[0] * self.width / im.shape[1])
-            im = cv2.resize(im, (self.width, height_temp))
-            top = (self.height - self.width) // 2
-            bottom = (self.height + self.width) // 2
-            im = im[top: bottom, :]
+        im = cv2.resize(im, (self.side_length, self.side_length))
 
         return im
 
-    def vis_warped_motion(self, arr_pos_1, arr_pos_2, output_mp4, fps, target_joint=kinect.WRIST_RIGHT):
+    def vis_motion_comp(self, arr_pos_1, arr_pos_2, output_mp4,
+                          fps, target_joint=kinect.WRIST_RIGHT):
 
         print("Processing")
 
         fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
-        writer = cv2.VideoWriter(output_mp4, fourcc, fps, (self.width * 2, self.height))
+        writer = cv2.VideoWriter(output_mp4, fourcc, fps, (self.side_length * 2, self.side_length))
 
         for i, j in tqdm(self.path_list[target_joint]):
 
@@ -112,9 +107,22 @@ class MotionDrawer():
 
             im = np.concatenate([im_1, im_2], axis=1)
 
+            dist = euclidean(arr_pos_1[i, target_joint, :], arr_pos_2[j, target_joint, :])
+            text = (
+                f"Target: {kinect.get_joint_name(target_joint)} | "
+                f"(i, j) = ({i}, {j}) | "
+                f"Distance: {dist:.3f}"
+            )
+            font_face = cv2.FONT_HERSHEY_PLAIN
+            font_scale = 1
+            thickness = 1
+            color = (0, 0, 0)
+            line_type = cv2.LINE_4
+
+            (w, h), baseline = cv2.getTextSize(text, font_face, font_scale, thickness)
             cv2.putText(
-                im, f"(i, j) = ({i}, {j})", (0, self.height),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv2.LINE_4
+                im, text, (baseline, h + 2 * baseline),
+                font_face, font_scale, color, thickness, line_type
             )
 
             writer.write(im)
@@ -156,6 +164,9 @@ class DTWDrawer():
 
 
 def main():
+    # ----------------------------------------------------------------------- #
+    # コマンド引数の定義
+    # ----------------------------------------------------------------------- #
     parser = argparse.ArgumentParser()
     parser.add_argument("input_json_1",
                         help="input json file path 1",
@@ -174,12 +185,21 @@ def main():
     if not os.path.isdir(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+    # ----------------------------------------------------------------------- #
+    # JSONファイルの読み込み
+    # ----------------------------------------------------------------------- #
     arr_time_1, _, arr_pos_1 = kinect.read_time_ori_pos(args.input_json_1, args.body_id_1)   # (T, J, D)
     arr_time_2, _, arr_pos_2 = kinect.read_time_ori_pos(args.input_json_2, args.body_id_2)
 
+    # ----------------------------------------------------------------------- #
+    # 身体の向きを考慮した座標変換
+    # ----------------------------------------------------------------------- #
     arr_pos_trans_1 = trans_coord(arr_pos_1)
     arr_pos_trans_2 = trans_coord(arr_pos_2)
 
+    # ----------------------------------------------------------------------- #
+    # 各関節の軌道に対して DTW の算出
+    # ----------------------------------------------------------------------- #
     dist_list = []
     path_list = []
 
@@ -200,23 +220,47 @@ def main():
             f, indent=4
         )
 
+    # ----------------------------------------------------------------------- #
+    # DTW スコアのグラフ描画
+    # ----------------------------------------------------------------------- #
     dtw_drawer = DTWDrawer(dist_list, path_list)
 
     scores_jpg = f"{OUTPUT_DIR}/scores.jpg"
     dtw_drawer.vis_scores(scores_jpg)
 
+
+    # ----------------------------------------------------------------------- #
+    # Warping path の描画
+    # ----------------------------------------------------------------------- #
     path_jpg_dir = f"{OUTPUT_DIR}/paths"
     if not os.path.isdir(path_jpg_dir):
         os.makedirs(path_jpg_dir)
 
     dtw_drawer.vis_paths(path_jpg_dir)
 
+    # ----------------------------------------------------------------------- #
+    # Waping path を考慮しない動きの比較
+    # ----------------------------------------------------------------------- #
     dif_time = arr_time_1[1:] - arr_time_1[:-1]
     mean_dif = np.mean(dif_time) / 1e6
     fps = 1 / mean_dif
 
-    motion_drawer = MotionDrawer(dist_list, path_list)
-    motion_drawer.vis_warped_motion(arr_pos_1, arr_pos_2, f"{OUTPUT_DIR}/compare.mp4", fps)
+    i_max = path_list[0][-1][0]
+    j_max = path_list[0][-1][1]
+    ij_max = max(i_max, j_max)
+    orig_path_list = [[[min(i, i_max), min(i, j_max)] for i in range(ij_max + 1)]]
+
+    motion_drawer = MotionDrawer(orig_path_list)
+    print("Drawing nonDTW graph")
+    motion_drawer.vis_motion_comp(arr_pos_1, arr_pos_2, f"{OUTPUT_DIR}/compare_nonDTW.mp4",
+                                  fps, target_joint=0)
+
+    # ----------------------------------------------------------------------- #
+    # Waping path を考慮した動きの比較
+    # ----------------------------------------------------------------------- #
+    motion_drawer = MotionDrawer(path_list)
+    print("Drawing DTW graph")
+    motion_drawer.vis_motion_comp(arr_pos_1, arr_pos_2, f"{OUTPUT_DIR}/compare_DTW.mp4", fps)
 
     print("Done")
 
